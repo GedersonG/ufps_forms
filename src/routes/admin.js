b0d495942f50164188ff24a1a1c7b622ba5075a4
dilansleytfm <gedersongustavogbot@ufps.edu.co>
2022-07-02 20:28:05 -0500

    utuk

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const adminController = require('../controllers/adminController');
 
 // Controladores admin
+// Aiuda ;(
 router.get('/', adminController.list)
 router.post('/sesion', adminController.sesion)
 router.get('/administrador', adminController.admin)
